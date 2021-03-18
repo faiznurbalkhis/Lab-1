62e56c73d012df09d080543f8cb743ba98f3af98
faiznurbalkhis <faiznurbalkhis96@gmail.com>
2021-03-18 22:54:05 +0800

    list weight

---
+++ Lab 1/src/listweight/Listofweight.java
@@ -0,0 +1,17 @@
+package listweight;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Listofweight {
+
+
+	  public static void main(String[] args) {
+	    ArrayList<String> weight = new ArrayList<String>();
+	    weight.add("earth");
+	    weight.add("mars");
+	   
+	    System.out.println(weight);
+	  }
+	
+	
+}
